1dc0ed7e5c1bb069e1c4db81b5ce9fcfe4fafbe9
Alexey Mityuryaev <103774038+gamxela@users.noreply.github.com>
2022-05-27 04:37:32 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = '5346682389:AAEDzrZiNRgIWY1d1ZGXAFn_6MqJnnwDub4'
+API_TOKEN = 'TOKEN'
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
